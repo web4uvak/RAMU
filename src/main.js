9d4a200abb1532ea49dcd08e06bbdd86419ceb6d
kkulaksyz <web4uvak@gmail.com>
2020-08-19 19:59:33 +0300

    Replace SemanticUI with ElementUI

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import SuiVue from 'semantic-ui-vue'
-import 'semantic-ui-css/semantic.min.css';
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 
-Vue.use(SuiVue)
+Vue.use(ElementUI)
 
 new Vue({
   render: h => h(App),
